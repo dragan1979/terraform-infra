e2be5bd28e0b1451ed6c75dea029ebc29850221e
Драган Вучановић <32413042+dragan1979@users.noreply.github.com>
2026-06-11 10:07:21 +0200

    Delete terraform/modules/database-internals directory

--- terraform/modules/database-internals/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "tags" {
-  type    = map(string)
-  default = {}
-}
-
-variable "app_user_password" {
-  type = string
-  description = "The password for the petclinic database user. This should be stored securely in Key Vault and passed in as a variable."
-}
