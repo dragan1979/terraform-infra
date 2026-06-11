9bb5b9faab2faf8afc8f85f29af2d2e37ad96d67
Драган Вучановић <32413042+dragan1979@users.noreply.github.com>
2026-06-11 10:05:34 +0200

    Delete terraform/modules/bastion/bastion_outputs.tf

--- terraform/modules/bastion/bastion_outputs.tf
+++
@@ -1,9 +0,0 @@
-output "bastion_id" {
-  value       = azurerm_bastion_host.bastion.id
-  description = "The ID of the Bastion Host"
-}
-
-output "public_ip_address" {
-  value       = azurerm_public_ip.bastion_pip.ip_address
-  description = "The Public IP address of the Bastion Host"
-}
